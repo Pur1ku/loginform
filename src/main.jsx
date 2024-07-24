37e757300f8ccab71daee265a191ffd16d33e13b
FEW <pfewddot@gmail.com>
2024-07-24 14:25:46 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import loginpage from './pages/loginpage'
+import AuthContextProvider from "./contexts/AuthContext.jsx";
+import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <AuthContextProvider>
   <React.StrictMode>
-    <loginpage />
-  </React.StrictMode>,
+    <App />
+  </React.StrictMode>
+</AuthContextProvider>
 )
